7f42d42ce0b2743e422917021671cd688b3baaa3
Dredex <root.doda@gmail.com>
2023-12-18 11:01:07 +0100

    Added more styling and event button

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 let highlight = document.querySelector('.highlight')
 
+function newItem() {
+    highlight.style.right = '59%';
+}
+
+function deleteItem() {
+    highlight.style.right = '-19%';
+}
