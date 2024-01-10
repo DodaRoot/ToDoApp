3786f8c6cd86c15cf50a4834985c72f294263d1d
Dredex <root.doda@gmail.com>
2024-01-10 19:18:25 +0100

    Start over

--- main.js
+++
@@ -1,9 +0,0 @@
-let highlight = document.querySelector('.highlight')
-
-function newItem() {
-    highlight.style.right = '59%';
-}
-
-function deleteItem() {
-    highlight.style.right = '-19%';
-}
