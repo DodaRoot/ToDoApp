54efd7fe78b635f5056847e88f1962a73a6d34a6
Dredex <root.doda@gmail.com>
2024-01-19 19:50:13 +0100

    Added List Functionality

--- main.js
+++ main.js
@@ -7,9 +7,37 @@ let mainObj = {
         1 : '12345' ,
         2 : 'None' ,
         3 : {
-            0 : 'List Name',
-            1 : ['Task One' , 'Time To Finnish'],
-            2 : ['Task Two' , 'Time To Finnish'],
+            0 : {
+                0 : 'List One' ,
+                1 : 'List Content'
+            },
+            1 : {
+                0 : 'List Two' ,
+                1 : 'List Content'
+            },
+            2 : {
+                0 : 'List Three' ,
+                1 : 'List Content'
+            },
+        },
+    } ,
+    1 : {
+        0 : 'Doda' ,
+        1 : '12345' ,
+        2 : 'None' ,
+        3 : {
+            0 : {
+                0 : 'List One Doda' ,
+                1 : 'List Content'
+            },
+            1 : {
+                0 : 'List Two Doda' ,
+                1 : 'List Content'
+            },
+            2 : {
+                0 : 'List Three Doda' ,
+                1 : 'List Content'
+            },
         },
     } ,
 }
@@ -22,26 +50,57 @@ else {
     localObj = JSON.parse(localStorage.getItem('mainObj'))
 }
 
+
+
+// CHECKING IF USER IS LOGGED IN AND SWITCHING THE PAGE
+let logged = localStorage.getItem('LOGGED')
+if (logged && document.URL.includes("index.html")) {
+    location.replace('list.html')
+}
+if (logged && document.URL.includes("list.html")) {
+    for (let list in localObj[logged][3] ) {
+        let contain = document.querySelector('.contain')
+        let card = document.createElement('div')
+        card.setAttribute('class' , 'card')
+        let title = document.createElement('div')
+        title.setAttribute('class' , 'title')
+        title.innerText = localObj[logged][3][list][0]
+        let deleted = document.createElement('div')
+        deleted.setAttribute('class' , 'delete')
+        deleted.innerText = '🗑️'
+        contain.append(card)
+        card.append(title)
+        card.append(deleted)
+        deleted.addEventListener('click' , (x) => {
+            contain.remove(x)
+            let old_data = JSON.parse(localStorage.getItem('mainObj'))
+            delete old_data[logged][3][list]
+            localStorage.setItem('mainObj' , JSON.stringify(old_data))
+            location.reload()
+        })
+    }
+}
+
 function signin () {
     event.preventDefault()
     let user = usernameForm.value
     let pass = passwordForm.value
     for (let index in localObj) {
         if (user == localObj[index][0] && pass == localObj[index][1]) {
-            console.log('Login Successful')
+            localStorage.setItem('LOGGED' , index)
+            alert('Welcome ' + user)
+            location.reload()
             break;
         }
         else if (user == localObj[index][0]) {
-            console.log('User exists but password is wrong')
+            alert('User exists but password is wrong')
             break;
         }
         else if (index == Object.keys(localObj).length - 1){
-            console.log('User does not exist')
+            alert('User does not exist')
         }
     }
 }
-
-console.log(localObj)
 
 function signup() {
     event.preventDefault()
@@ -60,17 +119,24 @@ function signup() {
     location.reload()
 }
 
+function signout() {
+    localStorage.removeItem('LOGGED')
+    location.replace('index.html')
+}
 
-
-
-// List
-let element = document.querySelectorAll('.card');
-let del = document.querySelectorAll('.delete')
-let i = 0
-// Selecting the specific lists
-element.forEach((x) => {
-    del[i].addEventListener('click' , () => {
-        x.style.display = 'none'
-    })
-    i++
-})
+function addNewList() {
+        // Setting up the new data
+        let new_data = {
+                0 : 'Other List Doda' ,
+                1 : 'List Content',
+        }
+        // Getting the value from obj and setting to temp value
+        let old_data = JSON.parse(localStorage.getItem('mainObj'))
+        // Getting the index of the temp obj
+        let index = (Object.keys(old_data[logged][3]).length)
+        // Setting the new value to temp obj
+        old_data[logged][3][index] = new_data
+        // Making the temp obj the primary obj
+        localStorage.setItem('mainObj' , JSON.stringify(old_data))
+        location.reload()
+}
