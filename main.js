ed50e26b37270cee4959f3162376d6944b1dc316
Dredex <root.doda@gmail.com>
2024-01-21 20:23:16 +0100

    Added a navigation system

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ let passwordForm = document.querySelector('#password')
 // Add Item Button
 let itemBtn = document.querySelector('.listContent button')
 // Modal and other hide and show comp
+let itemContainer = document.querySelectorAll('.listContent .contain div')
 let modal = document.querySelector('.modal')
 let userLists = document.querySelector('.userLists')
 let addNewBtn = document.querySelector('.userLists button')
 let span = document.querySelector('.modalContainer')
+let spanItem = document.querySelector('.modalContainerItem')
 let list = document.querySelectorAll('.card')
 
 let mainObj = {
@@ -160,7 +162,7 @@ function addNewList() {
     event.preventDefault()
     span.style.display = 'none'
     let listName = document.querySelector('#listName').value
-    let new_data = [listName , []]
+    let new_data = [listName]
     // Getting the value from obj and setting to temp value
     let old_data = JSON.parse(localStorage.getItem('mainObj'))
     // Setting the new value to temp obj
@@ -169,6 +171,7 @@ function addNewList() {
     let index = Object.keys(old_data[logged][3]).length
     // Making the temp obj the primary obj
     localStorage.setItem('mainObj' , JSON.stringify(old_data))        
+    localObj = JSON.parse(localStorage.getItem('mainObj'))
     // Appending the list
     appending(index - 1 , old_data , true)
 }
@@ -193,49 +196,95 @@ function appending(index , ref , animation) {
             itemBtn.style.display = 'flex'
         }
         cardLocation = index
-        // deleted.style.backgroundColor = 'rgb(0, 255, 0)'
         card.style.margin = '2.5% 0 0px 4.5%'
+        hideItems()
+        for (let item in ref[logged][3][index][1]) {
+            console.log(ref[logged][3][index][1][item][0])
+            appendingItem(item , localObj , false)
+        }
     })
     deleted.addEventListener('click' , () => {
         card.style.display = 'none'
         let old_data = JSON.parse(localStorage.getItem('mainObj'))
         old_data[logged][3].splice(index , 1)
         localStorage.setItem('mainObj' , JSON.stringify(old_data))
+        location.reload()
     })
     list = document.querySelectorAll('.card')
     addHideShow()
 }
-
-// Making the add new list modal
-
-window.addEventListener('click' , (x) => {
-    if (x.target == span) {
-        span.style.display = 'none'
-    }
-})
-
-// Adding show and hide item add functionality
-function addHideShow() {
-    for (let [index , card] of list.entries()) {
-        window.addEventListener('click' , (x) => {
-            if (x.target == userLists && x.target != card) {
-                itemBtn.style.display = 'none'
-                
-            }
-            if (x.target != card) {
-                let deleteBtn = document.querySelectorAll('.delete')[index]
-                // deleteBtn.style.backgroundColor = 'rgb(46, 48, 63)'
-                card.style.margin = '2.5% 0 0 2.5%'
-            }
-        })
-    }
-}
-addHideShow()
 
 function addNew () {
     span.style.display = 'flex'
 }
 
 function addNewItem() {
-    console.log(localObj[logged][3][cardLocation])
+    event.preventDefault()
+    spanItem.style.display = 'none'
+    let item = document.querySelector('#itemName').value
+    let new_data = [item , 2.50]
+    // Getting the value from obj and setting to temp value
+    let old_data = JSON.parse(localStorage.getItem('mainObj'))
+    // Setting the new value to temp obj
+    old_data[logged][3][cardLocation].push(new_data)
+    // Getting the index of the temp obj
+    let index = Object.keys(old_data[logged][3]).length
+    // Making the temp obj the primary obj
+    localStorage.setItem('mainObj' , JSON.stringify(old_data))        
+    localObj = JSON.parse(localStorage.getItem('mainObj'))
+    appendingItem(index - 1 , localObj , true)
 }
+
+function appendingItem(index , ref , animation) {
+    let contain = document.querySelector('.listContent .contain')
+    let card = document.createElement('div')
+    card.setAttribute('class' , 'card')
+    animation == true ? card.style.animation = 'fadeIn 1s' : false ;
+    let title = document.createElement('div')
+    title.setAttribute('class' , 'title')
+    title.innerText = ref[logged][3][index][0]
+    let deleted = document.createElement('div')
+    deleted.setAttribute('class' , 'delete')
+    deleted.innerText = '🗑️'
+    contain.append(card)
+    card.append(title)
+    card.append(deleted)
+    card.addEventListener('click' , (x) => {
+
+    })
+    deleted.addEventListener('click' , () => {
+        card.style.display = 'none'
+        let old_data = JSON.parse(localStorage.getItem('mainObj'))
+        old_data[logged][3][cardLocation].splice(index , 1)
+        localStorage.setItem('mainObj' , JSON.stringify(old_data))
+        location.reload()
+    })
+    itemContainer = document.querySelectorAll('.listContent .contain .card')
+}
+
+function addNewItemBtn() {
+    spanItem.style.display = 'flex'
+}
+
+// Making the add new list modal
+function hideItems() {
+    for(let item of itemContainer) {
+        item.style.display = 'none'
+    }
+}
+
+// Adding show and hide item add functionality
+function addHideShow() {
+    for (let card of list) {
+        window.addEventListener('click' , (x) => {
+            if (x.target == userLists && x.target != card) {
+                itemBtn.style.display = 'none'
+                hideItems()
+            }
+            if (x.target != card) {
+                card.style.margin = '2.5% 0 0 2.5%'
+            }
+        })
+    }
+}
+addHideShow()
